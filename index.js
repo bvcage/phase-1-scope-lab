890590d1d8288183793b7881504443ce5513cf5f
Bailey Cage <bailey.cage@gmail.com>
2022-08-09 11:38:41 -0400

    declare leastFavoriteCustomer and changeLeastFavoriteCustomer()

--- index.js
+++ index.js
@@ -11,3 +11,9 @@ function setBestCustomer () {
 function overwriteBestCustomer () {
     bestCustomer = 'maybe bob';
 }
+
+const leastFavoriteCustomer = 'john';
+
+function changeLeastFavoriteCustomer () {
+    leastFavoriteCustomer = 'dan';
+}
