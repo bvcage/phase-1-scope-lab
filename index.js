869030da43110da26bc2f5b090207c91508dfd79
Bailey Cage <bailey.cage@gmail.com>
2022-08-09 11:35:25 -0400

    declare overwriteBestCustomer()

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ function upperCaseCustomerName () {
     customerName = customerName.toUpperCase();
 }
 
-function setBestCustomer() {
+function setBestCustomer () {
     bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer () {
+    bestCustomer = 'maybe bob';
 }
